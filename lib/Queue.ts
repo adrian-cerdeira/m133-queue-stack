149901e2116394056908049bab277ad0716329c8
adrian_cerdeira <acwebdesign01@gmail.com>
2019-12-05 11:00:35 +0100

    site and return quere

--- lib/Queue.ts
+++ lib/Queue.ts
@@ -1,7 +1,10 @@
 import { IDataScructure } from "./IDataStructure";
 
 export class Queue implements IDataScructure {
+    public queue = [];
+    private offset = 0;
+
     public size(): number {
-        return undefined;
+        return (this.queue.length - this.offset);
     }
 }
