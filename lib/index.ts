7b64d51d833f015c9222a9dd2489292330744103
Adrian Cerdeira <acwebdesign01@gmail.com>
2019-12-08 17:19:45 +0100

    create p also in foreeach

--- lib/index.ts
+++ lib/index.ts
@@ -10,15 +10,15 @@ const container = document.getElementById('container');
 
 button.addEventListener('click', () => {
     const factory = new DataStructuresFactory();
-    const p = document.createElement('p');
     const isQueue = queueOption.spellcheck === true;
     const dataStructure = factory.create(isQueue ? DataStructures.Queue : DataStructures.Stack);
 
     dataStructure.enqueue(nameOfElement);
 
     dataStructure[isQueue ? 'queue' : 'stack'].forEach(q => {
+        const p = document.createElement('p');
         p.innerHTML += q;
+        container.appendChild(p);
     })
 
-    container.appendChild(p);
 });
