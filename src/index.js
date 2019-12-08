61d948e98a3b6235d14c5a48805f3abe45d5edfb
Adrian Cerdeira <acwebdesign01@gmail.com>
2019-12-08 21:49:10 +0100

    Element kann gelöscht werden

--- src/index.js
+++ src/index.js
@@ -5,21 +5,47 @@ import { DataStructures } from '../build/DataStructures';
 // Variables
 const queueOption = document.getElementById('queue');
 const nameOfElement = document.getElementById('name');
-const button = document.getElementById('button');
+
+const factory = new DataStructuresFactory();
+let isQueue = queueOption.checked === true;
+let dataStructure = factory.create(isQueue ? DataStructures.Queue : DataStructures.Stack);
+
+const btnAdd = document.getElementById('add');
+const btnDelete = document.getElementById('delete');
 const queueContainer = document.getElementById('queue-container');
 const stackContainer = document.getElementById('stack-container');
 
-button.addEventListener('click', () => {
-    const factory = new DataStructuresFactory();
-    const isQueue = queueOption.checked === true;
-    const dataStructure = factory.create(isQueue ? DataStructures.Queue : DataStructures.Stack);
+// Element hinzufügen, je nach Datenstruktur
+btnAdd.addEventListener('click', () => {
+    isQueue = queueOption.checked === true;
+    const queueOutputEmpty = isQueue && queueContainer.innerHTML === '';
+    const stackOutputEmpty = !isQueue && stackContainer.innerHTML === '';
+
+    if (queueOutputEmpty || stackOutputEmpty) {
+        dataStructure = factory.create(isQueue ? DataStructures.Queue : DataStructures.Stack);
+    }
 
     dataStructure.enqueue(nameOfElement.value);
 
+    output();
+});
+
+// Element löschen, je nach Datenstruktur
+btnDelete.addEventListener('click', () => {
+    dataStructure.poll();;
+    output();
+})
+
+// Ausgabe/Erneuerung der erstellten/gelöschten Daten
+function output() {
+    // Ausgabe zurücksetzen
+    queueContainer.innerHTML = '';
+    stackContainer.innerHTML = '';
+
+    // Ausgabe erstellen
     dataStructure[isQueue ? 'queue' : 'stack'].forEach(q => {
         const p = document.createElement('p');
         p.innerHTML += q;
         isQueue ? queueContainer.appendChild(p) : stackContainer.appendChild(p);
-    })
-
-});
+    });
+}
