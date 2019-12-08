c2f48bd3fa426b8e8bd5758f272aa89611595d53
Adrian Cerdeira <acwebdesign01@gmail.com>
2019-12-08 21:02:38 +0100

    Ausgabe in Stack und Queue

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import { DataStructures } from '../build/DataStructures';
 const queueOption = document.getElementById('queue');
 const nameOfElement = document.getElementById('name');
 const button = document.getElementById('button');
-const container = document.getElementById('container');
+const queueContainer = document.getElementById('queue-container');
+const stackContainer = document.getElementById('stack-container');
 
 button.addEventListener('click', () => {
     const factory = new DataStructuresFactory();
-    const isQueue = queueOption.spellcheck === true;
+    const isQueue = queueOption.checked === true;
     const dataStructure = factory.create(isQueue ? DataStructures.Queue : DataStructures.Stack);
 
     dataStructure.enqueue(nameOfElement.value);
@@ -18,7 +19,7 @@ button.addEventListener('click', () => {
     dataStructure[isQueue ? 'queue' : 'stack'].forEach(q => {
         const p = document.createElement('p');
         p.innerHTML += q;
-        container.appendChild(p);
+        isQueue ? queueContainer.appendChild(p) : stackContainer.appendChild(p);
     })
 
 });
