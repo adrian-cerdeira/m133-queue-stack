f669212e1a40adb45f8c3dd79aa2bdaa2f06df59
Adrian Cerdeira <acwebdesign01@gmail.com>
2019-12-10 20:29:50 +0100

    Bugfix: Elemente könne je nach Typ ohne Probleme gelöscht werden

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ const nameOfElement = document.getElementById('name');
 
 const factory = new DataStructuresFactory();
 let isQueue = queueOption.checked === true;
-let dataStructure = factory.create(isQueue ? DataStructures.Queue : DataStructures.Stack);
+let queue = factory.create(DataStructures.Queue);
+let stack = factory.create(DataStructures.Stack);
 
 const btnAdd = document.getElementById('add');
 const btnDelete = document.getElementById('delete');
@@ -21,10 +22,14 @@ btnAdd.addEventListener('click', () => {
     const queueOutputEmpty = isQueue && queueContainer.innerHTML === '';
     const stackOutputEmpty = !isQueue && stackContainer.innerHTML === '';
 
-    if (queueOutputEmpty || stackOutputEmpty) {
-        dataStructure = factory.create(isQueue ? DataStructures.Queue : DataStructures.Stack);
+    // Objekt erstellen, falls noch nicht existent
+    if (queueOutputEmpty) {
+        queue = factory.create(DataStructures.Queue)
+    } else if (stackOutputEmpty) {
+        stack = factory.create(DataStructures.Stack)
     }
 
+    const dataStructure = isQueue ? queue : stack;
     dataStructure.enqueue(nameOfElement.value);
 
     output();
@@ -32,17 +37,22 @@ btnAdd.addEventListener('click', () => {
 
 // Element löschen, je nach Datenstruktur
 btnDelete.addEventListener('click', () => {
+    isQueue = queueOption.checked === true;
+    const dataStructure = isQueue ? queue : stack;
     dataStructure.poll();
+
     output();
 })
 
 // Ausgabe/Erneuerung der erstellten/gelöschten Daten
 function output() {
     // Ausgabe zurücksetzen
-    queueContainer.innerHTML = '';
-    stackContainer.innerHTML = '';
+    isQueue
+        ? queueContainer.innerHTML = ''
+        : stackContainer.innerHTML = '';
 
     // Ausgabe erstellen
+    const dataStructure = isQueue ? queue : stack;
     dataStructure[isQueue ? 'queue' : 'stack'].forEach(q => {
         const p = document.createElement('p');
         p.innerHTML += q;
