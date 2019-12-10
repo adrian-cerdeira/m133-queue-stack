e3bfd6f88afda0a50d297d2f59ffc822454fecb7
Adrian Cerdeira <acwebdesign01@gmail.com>
2019-12-10 21:04:49 +0100

    delete build

--- build/DataStructures.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var DataStructures;
-(function (DataStructures) {
-    DataStructures["Queue"] = "FIFO";
-    DataStructures["Stack"] = "LIFO";
-})(DataStructures = exports.DataStructures || (exports.DataStructures = {}));
-;
